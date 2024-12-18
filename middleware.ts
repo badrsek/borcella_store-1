f14b9d8d6a779afcb4b0e01a6a380d1a61185b7f
badrsek <72362138+badrsek@users.noreply.github.com>
2024-12-18 16:48:41 +0100

    changes

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,8 @@
-import { withClerkMiddleware } from "@clerk/nextjs";
 import { authMiddleware } from "@clerk/nextjs";
-
-export default withClerkMiddleware({
-  publicRoutes: ["/:path*"],
+ 
+export default authMiddleware({
+  publicRoutes: ["/:path*"]
 });
-console.log(authMiddleware);
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
